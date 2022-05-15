c0ecf3b1220d057105c19adf1615367b83bf2068
Junhyeok <83490421+wnsgur9137@users.noreply.github.com>
2022-05-15 14:34:02 +0900

    week11-2

--- file_server.c
+++ file_server.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <strng.h>
+#include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -25,6 +25,39 @@ int main(int argc, char *argv[]) {
 
         fp=fopen("file_server.c", "rb");
         serv_sd=socket(PF_INET, SOCK_STREAM, 0);
-        
-        
+
+        memset(&serv_adr, 0, sizeof(serv_adr));
+        serv_adr.sin_family=AF_INET;
+        serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+        serv_adr.sin_port=htons(atoi(argv[1]));
+
+        bind(serv_sd, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
+        listen(serv_sd, 5);
+
+        clnt_adr_sz=sizeof(clnt_adr);
+        clnt_sd=accept(serv_sd, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+
+        while(1) {
+                read_cnt=fread((void*)buf, 1, BUF_SIZE, fp);
+                if(read_cnt<BUF_SIZE) {
+                        write(clnt_sd, buf, read_cnt);
+                        break;
+                }
+                write(clnt_sd, buf, BUF_SIZE);
+        }
+
+        shutdown(clnt_sd, SHUT_WR);
+        read(clnt_sd, buf, BUF_SIZE);
+        printf("Message from clint: %s \n", buf);
+
+        fclose(fp);
+        close(clnt_sd);
+        close(serv_sd);
+        return 0;
+}
+
+void error_handling(char *message) {
+        fputs(message, stderr);
+        fputc('\n', stderr);
+        exit(1);
 }
